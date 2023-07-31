125237da1575d2751dbc3178fbf7d6a5d0f9ee7e
Alex Skobelev <alex@alazy.dev>
2023-07-31 05:43:04 +0000

    warn/confirm top level question deletion

--- src/resources/views/configuration/question/partials/actions.blade.php
+++ src/resources/views/configuration/question/partials/actions.blade.php
@@ -2,7 +2,20 @@
     <a href="{{ route('seat-hr.config.question.edit', ['id' => $row->id]) }}" class="btn btn-sm btn-warning">
         <i class="fas fa-pencil-alt"></i>
     </a>
-    <a href="{{ route('seat-hr.config.question.delete', ['id' => $row->id]) }}" class="btn btn-sm btn-danger">
+    <a onclick="return confirmQuestionDelete(this)" 
+        href="{{ route('seat-hr.config.question.delete', ['id' => $row->id]) }}" 
+        class="btn btn-sm btn-danger"
+    >
         <i class="fas fa-trash"></i>
     </a>
 </div>
+<script>
+    function confirmQuestionDelete(node) {
+        return confirm(
+            'DANGER: Deleting a question will delete all historical answers for the question!' +
+            '\nThis will also lead to existing applications having this question/answer deleted, potentially leading to blank applications. ' +
+            '\nYou should use the deactivate option instead to keep answers but prevent future submissions.' +
+            '\n\nAre you sure you want to proceed?'
+        );
+    }
+</script>
